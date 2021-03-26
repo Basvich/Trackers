fb6af2d8acff1bd71ca2fa9b98a447ae2d165fb7
Basilio <basilio.alvarez@gmail.com>
2021-03-26 21:24:50 +0100

    include p5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as p5 from 'p5';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'trackers';
+  private canvasP5: p5;
+
+  ngOnInit(): void {
+    this.setupP5();    
+  }
+  setupP5() {
+    const sketch = (s) => {
+      s.preload = () => {
+        // preload code
+      };
+      s.setup = () => {
+        const cc = s.createCanvas(800, 400);
+        cc.parent('containerP5');
+      };
+    };
+    this.canvasP5 = new p5(sketch);
+    this.canvasP5.noLoop();  // Se desactiva el bucle sobre la funcion draw
+    this.canvasP5.draw = () => {
+      this.draw();
+    };
+  }
+
+  protected draw() {
+    this.canvasP5.background(250); 
+  }
 }
