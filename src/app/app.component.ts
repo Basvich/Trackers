6aba5299ba15da745f9a6214de8b90bc7b778dc0
Basilio <basilio.alvarez@gmail.com>
2021-03-28 17:55:04 +0200

    Mouse Whell zoom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import * as p5 from 'p5';
 import {VTracker} from './visual-tracker/VTracker';
 
@@ -7,17 +7,21 @@ import {VTracker} from './visual-tracker/VTracker';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { 
+export class AppComponent {
   private canvasP5: p5;
-  private trackers: VTracker[]= [];
+  private trackers: VTracker[] = [];
+
+  private zoom = 1;
+  private tx = -20;
+  private ty = 0;
   title = 'trackers';
   ngOnInit(): void {
-    this.setupP5();    
+    this.setupP5();
     this.setupTrackers();
   }
- 
 
-  setupP5():void {
+
+  setupP5(): void {
     const sketch = (s) => {
       s.preload = () => {
         // preload code
@@ -32,32 +36,58 @@ export class AppComponent {
     this.canvasP5.draw = () => {
       this.draw();
     };
+    this.canvasP5.mouseWheel = (event: Record<string, unknown>) => {
+      if (event.target != (this.canvasP5 as any).drawingContext.canvas)
+        return true;
+      const x0 = <number>(event.offsetX);
+      const y0 = <number>(event.offsetY);     
+      const z0 = this.zoom;
+      this.zoom += 0.001 * <number>event.delta;
+      this.zoom = this.canvasP5.constrain(this.zoom, 0.05, 8);
+      const zz=(1/this.zoom-1/z0);
+      this.tx = x0*zz+this.tx;
+      this.ty = y0*zz+this.ty;
+      this.invalidateDrawn();
+      return false;
+    };
+
+    this.canvasP5.mouseDragged = (event: Record<string, unknown>) => {
+      if (event.target != ((this.canvasP5) as any).drawingContext.canvas)
+        return true;
+      this.tx = <number>event.offsetX;
+      this.ty = <number>event.offsetY;
+      this.invalidateDrawn();
+      return false;
+    };
+    this.canvasP5.mouseMoved
   }
 
-  public invalidateDrawn(): void{
-    this.canvasP5.redraw();
+  public invalidateDrawn(): void {
+    setTimeout(() => this.canvasP5.redraw(), 0);
   }
 
   protected draw(): void {
-    this.canvasP5.background(250); 
+    this.canvasP5.background(250);
+    this.canvasP5.scale(this.zoom);
+    this.canvasP5.translate(this.tx, this.ty);
     this.trackers.forEach(element => {
       element.drawn(this.canvasP5);
     });
   }
 
-  protected setupTrackers(): void{
-    const f=10;
-    let y=20;
-    let x=20;
-    for(let i=0; i<f ;i++){
-      const nTrack=new VTracker(x,y);
-      x+=40;
-      if(x>400){
-        x=0;
-        y+=40;
+  protected setupTrackers(): void {
+    const f = 100;
+    let y = 20;
+    let x = 20;
+    for (let i = 0; i < f; i++) {
+      const nTrack = new VTracker(x, y);
+      x += 40;
+      if (x > 400) {
+        x = 20;
+        y += 40;
       }
-    this.trackers.push(nTrack);
+      this.trackers.push(nTrack);
     }
-    
+
   }
 }
